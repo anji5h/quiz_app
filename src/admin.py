a38eae1bdea3b859bd2fdf82c0a42d71d385e0da
anjish <40964308+anji5h@users.noreply.github.com>
2025-04-22 01:52:21 +0300

    added add_question function

--- src/admin.py
+++ src/admin.py
@@ -1,31 +1,69 @@
 import json
 import os
-from schemas import QuizConfig
+import uuid
+from schemas import QuizConfig, Quiz, Question, Topic
 
 CONFIG_DIR = "src/static"
-QUIZ_CONFIG_FILE_PATH = os.path.join(CONFIG_DIR, "admin.json")
-QUESTION_FILE_PATH = os.path.join(CONFIG_DIR, "questions.json")
+ADMIN_FILE_PATH = os.path.join(CONFIG_DIR, "admin.json")
+QUIZ_FILE_PATH = os.path.join(CONFIG_DIR, "questions.json")
+
 
 def print_topics(topics: list[str]):
-    print("Select the quiz topic: ")
+    print("\nSelect the quiz topic: ")
     for idx, topic in enumerate(topics, 1):
         print(f"Topic {idx}: {topic}")
 
+
+def add_question(
+    ques_per_topic: int,
+    topic_name: str,
+):
+    with open(QUIZ_FILE_PATH, "r") as file:
+        raw_data = file.read()
+        quiz = Quiz.model_validate_json(raw_data)
+
+    if topic_name not in quiz.root:
+        quiz.root[topic_name] = Topic(root={})
+
+    topic = quiz.root[topic_name]
+
+    if len(topic.root) >= ques_per_topic:
+        print("Already 10 questions")
+        return
+
+    ques = input("\nEnter your question: ")
+    opt_a = input("Enter first option (a): ")
+    opt_b = input("Enter second option (b): ")
+    opt_c = input("Enter third option (c): ")
+    opt_d = input("Enter fourth option (d): ")
+    ans = input("Enter correct option (a, b, c, d): ")
+
+    new_question = Question(
+        question=ques, a=opt_a, b=opt_b, c=opt_c, d=opt_d, answer=ans
+    )
+    ques_id = str(uuid.uuid4())
+
+    topic.root[ques_id] = new_question
+
+    with open(QUIZ_FILE_PATH, "w") as f:
+        json.dump(quiz.model_dump(), f, indent=4)
+
+
+
 def main():
     try:
-        with open(QUIZ_CONFIG_FILE_PATH, "r") as file:
-            raw_config = json.load(file)
-
-        quiz_config = QuizConfig(**raw_config)
+        with open(ADMIN_FILE_PATH, "r") as file:
+            raw_data = json.load(file)
+            config = QuizConfig(**raw_data)
 
         password = input("Enter app password: ").strip()
 
-        if password != quiz_config.appPassword:
+        if password != config.appPassword:
             print("Wrong password")
             return
-        
+
         while True:
-            print("Select a task below:")
+            print("\nSelect a task below:")
             print("Task-1: Add a question to topic")
             print("Task-2: Delete a question of topic")
             print("Task-3: Exit the program")
@@ -33,20 +71,14 @@ def main():
 
             match task:
                 case 1:
-                    print_topics(quiz_config.categories)
+                    print_topics(config.categories)
                     topic_no = int(input("Enter the topic number: ").strip())
-                    topic = quiz_config.categories[topic_no - 1]
-
-                    question = input("Enter your question: ")
-                    option_a = input("Enter first option (a): ")
-                    option_b = input("Enter second option (b): ")
-                    option_c = input("Enter third option (c): ")
-                    option_d = input("Enter fourth option (d): ")
-                    answer = input("Enter correct option (a, b, c, d): ")
+                    topic = config.categories[topic_no - 1]
+                    add_question(ques_per_topic=config.questionsPerTopic, topic=topic)
                 case 2:
-                    print_topics(quiz_config.categories)
+                    print_topics(config.categories)
                     topic_no = int(input("Enter the topic number: ").strip())
-                    topic = quiz_config.categories[topic_no - 1]
+                    topic = config.categories[topic_no - 1]
                 case _:
                     break
 
--- src/schemas.py
+++ src/schemas.py
@@ -1,7 +1,27 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, RootModel, field_validator
+from typing import List, Dict
 
+
+# schema for admin.json
 class QuizConfig(BaseModel):
     appPassword: str
     categories: List[str]
     questionsPerTopic: int
+
+
+# schema for questions.json
+class Question(BaseModel):
+    question: str
+    a: str
+    b: str
+    c: str
+    d: str
+    answer: str
+
+
+class Topic(RootModel[Dict[str, Question]]):
+    pass
+
+
+class Quiz(RootModel[Dict[str, Topic]]):
+    pass
